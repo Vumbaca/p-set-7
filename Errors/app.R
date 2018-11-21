a8878bb7158ca784fcb0cd52810cbe79e33b63aa
Max Vumbaca <vumbaca@college.harvard.edu>
2018-11-21 00:36:17 -0500

    Plot creation

--- Errors/app.R
+++ Errors/app.R
@@ -10,7 +10,7 @@
 library(shiny)
 library(tidyverse)
 
-midterms <- read_rds("./Errors/shiny_data.rds")
+midterms <- read_rds("shiny_data.rds")
 
 ui <- fluidPage(
    
@@ -22,7 +22,7 @@ ui <- fluidPage(
       ),
       
       mainPanel(
-         plotOutput("plot")
+        plotOutput("plot")
       )
    )
 )
@@ -30,7 +30,90 @@ ui <- fluidPage(
 server <- function(input, output) {
    
    output$plot <- renderPlot({
-ggplot
+     if(input$demo == "Age"){
+       ggplot(midterms, aes(x = p_18, y = perc_error)) +
+         geom_point(color = "blue") +
+         geom_smooth(color = "blue") +
+         ylim(-10, 10)
+       # ggplot(midterms, aes(x = p_30, y = perc_error)) +
+       #   geom_point(color = "red") +
+       #   geom_smooth(color = "red") +
+       #   ylim(-10, 10)
+       # ggplot(midterms, aes(x = p_45, y = perc_error)) +
+       #   geom_point(color = "green") +
+       #   geom_smooth(color = "green") +
+       #   ylim(-10, 10)
+       # ggplot(midterms, aes(x = p_65, y = perc_error)) +
+       #   geom_point(color = "purple") +
+       #   geom_smooth(color = "purple") +
+       #   ylim(-10, 10)
+       # ggtitle("Title") xlab("X") ylab("Y")
+     }
+     else{
+       if(input$demo == "Education"){
+         ggplot(midterms, aes(x = p_high, y = perc_error)) +
+           geom_point(color = "blue") +
+           geom_smooth(color = "blue") +
+           ylim(-10, 10)
+         ggplot(midterms, aes(x = p_some, y = perc_error)) +
+           geom_point(color = "red") +
+           geom_smooth(color = "red") +
+           ylim(-10, 10)
+         ggplot(midterms, aes(x = p_coll, y = perc_error)) +
+           geom_point(color = "green") +
+           geom_smooth(color = "green") +
+           ylim(-10, 10)
+         ggplot(midterms, aes(x = p_post, y = perc_error)) +
+           geom_point(color = "purple") +
+           geom_smooth(color = "purple") +
+           ylim(-10, 10)
+       }
+       else{
+          if(input$demo == "Race"){
+           ggplot(midterms, aes(x = p_white, y = perc_error)) +
+             geom_point(color = "blue") +
+             geom_smooth(color = "blue") +
+             ylim(-10, 10)
+           ggplot(midterms, aes(x = p_hispanic, y = perc_error)) +
+             geom_point(color = "red") +
+             geom_smooth(color = "red") +
+             ylim(-10, 10)
+           ggplot(midterms, aes(x = p_black, y = perc_error)) +
+             geom_point(color = "green") +
+             geom_smooth(color = "green") +
+             ylim(-10, 10)
+           ggplot(midterms, aes(x = p_asian, y = perc_error)) +
+             geom_point(color = "purple") +
+             geom_smooth(color = "purple") +
+             ylim(-10, 10)
+          }
+          else{
+            if(input$demo == "Gender"){
+             ggplot(midterms, aes(x = p_male, y = perc_error)) +
+               geom_point(color = "blue") +
+               geom_smooth(color = "blue") +
+               ylim(-10, 10)
+             ggplot(midterms, aes(x = p_female, y = perc_error)) +
+               geom_point(color = "red") +
+               geom_smooth(color = "red") +
+               ylim(-10, 10)
+            }
+            else{
+             if(input$demo == "Party"){
+               ggplot(midterms, aes(x = p_dem, y = perc_error)) +
+                 geom_point(color = "blue") +
+                 geom_smooth(color = "blue") +
+                 ylim(-10, 10)
+               ggplot(midterms, aes(x = p_rep, y = perc_error)) +
+                 geom_point(color = "red") +
+                 geom_smooth(color = "red") +
+                 ylim(-10, 10)
+               ggplot(midterms, aes(x = p_indep, y = perc_error)) +
+                 geom_point(color = "green") +
+                 geom_smooth(color = "green") +
+                 ylim(-10, 10)
+             }
+}}}}
    })
 }
 
